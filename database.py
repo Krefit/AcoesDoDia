069f51fb8713b5682d8596230bd94b99790e19d7
Krefit <gustavokrawczyk@gmail.com>
2024-08-23 02:50:34 -0300

    Criação de inserção no banco v0.1

---
+++ database.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+class StocksDB:
+    def __init__(self, host, port, database, user, password):
+        self.conn = psycopg2.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=user,
+            password=password
+        )
+        self.cur = self.conn.cursor()
+
+    def insert_stock(self, symbol, company_name, open_price, high_price, low_price, close_price, volume, date):
+        # Check if the stock symbol and date already exist
+        self.cur.execute("SELECT COUNT(*) FROM stocks WHERE symbol = %s AND date = %s", (symbol, date))
+        count = self.cur.fetchone()[0]
+        if count > 0:
+            print(f"Stock with symbol {symbol} and date {date} already exists in the database.")
+            return
+
+        # Insert the new stock
+        sql = "INSERT INTO stocks (symbol, company_name, open_price, high_price, low_price, close_price, volume, date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        self.cur.execute(sql, (symbol, company_name, open_price, high_price, low_price, close_price, volume, date))
+        self.conn.commit()
+        print(f"Inserted new stock with symbol {symbol} and date {date}.")
+
+    def get_all_stocks(self):
+        self.cur.execute("SELECT * FROM stocks")
+        return self.cur.fetchall()
+
+    def close_connection(self):
+        self.conn.close()
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import requests
+from database import StocksDB
 from datetime import datetime
 
 # Store API URL
 url = 'https://brapi.dev/api/quote/PETR4?range=3mo&interval=1d&token=vTodNNnn1UMPvgBD1Vg7dB'
+cn = StocksDB(
+    host="localhost",
+    port=5433,
+    database="acao_dia",
+    user="postgres",
+    password="postgres"
+)
 
 # Assign the headers
 headers = {'Accept': 'application/brapi+json'}
@@ -23,6 +31,7 @@ if response.status_code == 200:
 
     # Extract symbol, company name, and historical data prices from each result
     for result in results:
+        #symbol, company_name, open_price, high_price, low_price, close_price, volume, date
         symbol = result.get("symbol", "N/A")
         company_name = result.get("longName", "N/A")
         historical_data_prices = result.get("historicalDataPrice", [])
@@ -30,12 +39,19 @@ if response.status_code == 200:
         # Extract historical data prices as a list of dictionaries
         prices = []
         for price in historical_data_prices:
+            
             date = price.get("date", "N/A")
             open_price = price.get("open", "N/A")
+            high_price = price.get("high", "N/A")
+            low_price = price.get("low", "N/A")
+            close_price = price.get("close", "N/A")
+            volume = price.get("volume", "N/A")
             formatted_date = datetime.fromtimestamp(date).strftime('%d/%m/%Y')
             prices.append({"date": formatted_date, "value": open_price})
+            cn.insert_stock(symbol, company_name, open_price, high_price, low_price, close_price, volume, formatted_date)
 
         stock_data.append({"symbol": symbol, "company_name": company_name, "prices": prices})
+        
 
     # Print the stock data
     for stock in stock_data:
